40518aa8ee887098ba2577042a2a527665df00a2
BurdiApps <aburdick@byupathway.edu>
2026-03-28 15:13:59 -0700

    Add product list sort by name and price

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -33,9 +33,26 @@ export default class ProductList {
     this.category = category;
     this.dataSource = dataSource;
     this.listElement = listElement;
+    this.products = [];
   }
   async init() {
-    const list = await this.dataSource.getData();
+    this.products = await this.dataSource.getData();
+    this.renderList(this.products);
+  }
+  renderList(list) {
     renderListWithTemplate(productCardTemplate, this.listElement, list, "afterbegin", true);
+  }
+  sort(sortBy) {
+    let sorted;
+    if (sortBy === "name") {
+      sorted = [...this.products].sort((a, b) =>
+        a.NameWithoutBrand.localeCompare(b.NameWithoutBrand)
+      );
+    } else if (sortBy === "price") {
+      sorted = [...this.products].sort((a, b) => a.FinalPrice - b.FinalPrice);
+    } else {
+      sorted = [...this.products];
+    }
+    this.renderList(sorted);
   }
 }
