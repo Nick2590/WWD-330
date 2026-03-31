5f562a7bf36e849216fe9c87cdbec2ec0a33787d
BurdiApps <aburdick@byupathway.edu>
2026-03-30 18:56:21 -0700

    Individual W04: Error checking, validation, styled alert messages

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -101,6 +101,25 @@ export function updateCartCount() {
   }
 }
 
-export function alertMessage(message, duration = 3000) {
-  alert(message);
+export function alertMessage(message, scroll = true) {
+  const alert = document.createElement("div");
+  alert.classList.add("alert");
+  alert.innerHTML = `<p>${message}</p><span>X</span>`;
+
+  alert.addEventListener("click", function (e) {
+    if (e.target.tagName == "SPAN") {
+      main.removeChild(this);
+    }
+  });
+
+  const main = document.querySelector("main");
+  main.prepend(alert);
+
+  if (scroll) window.scrollTo(0, 0);
+}
+
+export function removeAllAlerts() {
+  const alerts = document.querySelectorAll(".alert");
+  alerts.forEach((alert) => document.querySelector("main").removeChild(alert));
+}
 }
